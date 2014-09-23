42510fe1f800dbf558f246e666206b8e5d1a29b1
Nico Gestest <nxgstest@hotmail.com>
2014-09-23 19:34:51 -0300

    Update one.rb

--- one.rb
+++ one.rb
@@ -1,7 +1,7 @@
 # Test class, what else?
-class Test
-  def initialize; puts self; end
-  def self.test puts self; end;
+class Test;
+  def initialize; puts self; end;
+  def self.test puts self; end;;
   puts self
 end
 #end comment!
